c28ce1ef309b7149ef0545e6678dfd6f31b3c2ca
YoneCode <yonecode@users.noreply.github.com>
2026-06-11 02:20:46 +0000

    vite config

---
+++ frontend/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  server: {
+    host: "127.0.0.1",
+    port: 5173,
+    strictPort: true,
+  },
+  preview: {
+    host: "127.0.0.1",
+    port: 5173,
+    strictPort: true,
+  },
+  build: {
+    outDir: "dist",
+    target: "es2020",
+  },
+});
